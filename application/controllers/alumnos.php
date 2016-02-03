2c3909f5dc875c898eefc3c3ab203bbb24ecc795
keepercito <keepercito@gmail.com>
2016-02-03 08:45:04 -0600

    esqueleto vista y controlador agregar alumnos

---
+++ application/controllers/alumnos.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Alumno extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->helper('form');
+		$this->load->view('plantillas/header');
+		$this->load->view('plantillas/sidebar');
+		$this->load->view('front_end/agregar_alumno');
+		$this->load->view('plantillas/footer');
+	}
+}
---
+++ application/views/front_end/agregar_alumno.php
@@ -0,0 +1,31 @@
+<section>
+	<?= form_open()?>
+		<label for="">
+		Nombre: 				  <input type="text" id="nombres"><br>
+		</label>
+		<label for="">
+		Apellidos: 				  <input type="text" id="apellidos"><br>
+		</label>
+		<label for="">
+		Nivel: 					  <select name="" id="nivel"></select><br>	
+		</label>
+		<label for="">
+		Dirección Personal: 	  <input type="text" id="direccion"><br>	
+		</label>
+		<label for="">
+		Telefono: 				  <input type="text" id="telefono"><br>	
+		</label>
+		<label for="">
+		Nombre del Padre: 		  <input type="text" id="papa"><br>	
+		</label>
+		<label for="">
+		Nombre del Madre: 		  <input type="text" id="mama"><br>	
+		</label>
+		<label for="">
+		Nombre del Responsable:   <input type="text" id="responsable"><br>	
+		</label>
+		<label for="">
+		Telefono del Responsable: <input type="text" id="telRes"><br>
+		</label>
+	<?= form_close()?>
+</section>

15c4054da7a6029418d9cbc1f4081c7b86d67110
keepercito <keepercito@gmail.com>
2016-02-15 23:30:39 -0600

    buscar alumnos en una sola vista

--- application/views/front_end/ficha_alumno.php
+++ application/views/front_end/ficha_alumno.php
@@ -1,96 +1,136 @@
 <section class="contenido">
-	<h1>Datos del alumno</h1>
-	<?= form_open()?>
-		<table class="tabla">
-			<tbody>
-				<tr>
-					<td style="{font-color:red;}">Nombre del alumno:</td>
-					<td>Apellidos del alumno:</td>
-				</tr>
-				<tr>
-					<td>Juan Antonio</td>
-					<td>Pérez Reyes</td>
-				</tr>
-				<tr>
-					<td>Fecha de nacimiento:</td>
-					<td>Nivel:</td>
-				</tr>
-				<tr>
-					<td>21/04/1998</td>					
-					<td>Nivel 2</select></td>
-				</tr>
-				<tr>
-					<td>Dirección de Residencia:</td>
-					<td><?= form_input('dir')?></td>
-				</tr>
-				<tr>
-					<td>Telefono:</td>
-					<td><?= form_input('tel')?></td>
-				</tr>
-				<tr>
-					<td>Nombre de la Madre:</td>
-					<td><?= form_input('madre')?></td>
-				</tr>
-				<tr>
-					<td>DUI de la madre:</td>
-					<td><?= form_input('duim')?></td>
-				</tr>
-				<tr>
-					<td>Profesión de la madre:</td>
-					<td><?= form_input('profm')?></td>
-				</tr>
-				<tr>
-					<td>Lugar de trabajo:</td>
-					<td><?= form_input('tbjm')?></td>
-				</tr>
-				<tr>
-					<td>Telefóno de la madre:</td>
-					<td><?= form_input('telm')?></td>
-				</tr>
-				<tr>
-					<td>Nombre del Padre:</td>
-					<td><?= form_input('padre')?></td>
-				</tr>
-				<tr>
-					<td>Documento Unico de identidad del padre:</td>
-					<td><?= form_input('duip')?></td>
-				</tr>
-				<tr>
-					<td>Profesión del padre:</td>
-					<td><?= form_input('profp')?></td>
-				</tr>
-				<tr>
-					<td>Lugar de trabajo:</td>
-					<td><?= form_input('tbjp')?></td>
-				</tr>
-				<tr>
-					<td>Telefóno del padre:</td>
-					<td><?= form_input('telp')?></td>
-				</tr>
-				<tr>
-					<h3>En caso de no contar con los padres:</h3>
-				</tr>
-				<tr>
-					<td>Responsable:</td>
-					<td><?= form_input('resp')?></td>
-				</tr>
-				<tr>
-					<td>Documento Unico de Identidad del responsable:</td>
-					<td><?= form_input('duir')?></td>
-				</tr>
-				<tr>
-					<td>Telefono del Responsable:</td>
-					<td><?= form_input('telr')?></td>
-				</tr>
-				<tr>
-					<td>Padecimientos del inscrito:</td>
-					<td><?= form_textarea('padecimiento')?></td>
-				</tr>
-				<tr>
-					<td>Medicinas:</td>
-					<td><?= form_textarea('medic')?></td>
-				</tr>
-			</tbody>
-		</table>
-	<?= form_close()?>
+	<?php if(isset($_SESSION['logged_in'])): #si ha iniciado sesion se mostrara el contenido sino el mensaje de error?>
+		<h1>Datos del alumno</h1>
+		<?= form_open('Alumnos/actualizarAlumno')?>
+			<table class="tabla">
+				<tbody>
+					<tr>
+						<td style="{font-color:red;}">Nombre del alumno:</td>
+						<td>Apellidos del alumno:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getNombres();?></td>
+						<td><?php echo $alumno->getApellidos();?></td>
+					</tr>
+					<tr>
+						<td>Estatura:</td>
+						<td>Peso:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getEstatura();?></td>
+						<td><?php echo $alumno->getPeso();?></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Fecha de nacimiento:</td>
+						<td>Genero: Nivel:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getFechaNacimiento();?></td>				
+						<td><?php echo $alumno->getGenero();?>
+							<?php echo $alumno->getIdNivel();?>
+						</td>
+					</tr>
+					<tr>
+						<td>Dirección de Residencia:</td>
+						<td>Telefono:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getDireccion();?></td>
+						<td><?php echo $alumno->getTelefono();?></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Nombre de la Madre:</td>
+						<td>DUI de la madre:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getNombreMadre();?></td>
+						<td><?php echo $alumno->getDuiMadre();?></td>
+					</tr>
+					<tr>
+						<td>Lugar de trabajo:</td>
+						<td>Telefóno de la madre:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getTrabajoMadre();?></td>
+						<td><?php echo $alumno->getTelMadre();?></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Nombre del Padre:</td>
+						<td>DUI del padre:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getNombrePadre();?></td>
+						<td><?php echo $alumno->getDuiPadre();?></td>
+					</tr>
+					<tr>
+						<td>Lugar de trabajo:</td>
+						<td>Telefóno del padre:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getTrabajoPadre();?></td>
+						<td><?php echo $alumno->getTelPadre();?></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Experiencia previa:</td>
+						<td>Estado:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getExpPrevia();?></td>
+						<td><?php echo $alumno->getEstado();?></td>
+					</tr>
+					<tr>
+						<td colspan="2">En caso de no contar con los padres mencionar un responsable:</td>
+					</tr>
+					<tr>
+						<td>Nombre del responsable:</td>
+						<td>DUI del responsable:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getNombreResp();?></td>
+						<td><?php echo $alumno->getDuiResp();?></td>
+					</tr>
+					<tr>
+						<td>Lugar de trabajo:</td>
+						<td>Telefono del Responsable:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getTrabajoResp();?></td>
+						<td><?php echo $alumno->getTelResp();?></td>
+					</tr>
+					<tr>
+						<td>Padecimientos del inscrito:</td>
+						<td>Medicinas:</td>
+					</tr>
+					<tr>
+						<td><?php echo $alumno->getPadecimientos();?></td>
+						<td><?php echo $alumno->getMedicamentos();?></td>
+					</tr>
+					<tr>
+						<td><center><input type="button" value="Volver atrás" name="volver atrás2" onclick="history.back()" /></center></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		<?= form_close()?>
+	<?php else: ?>
+		<br><br>
+		<h2>Tu sesión expiró o no has iniciado sesión, por favor inicia sesión para ver este contenido</h2>
+		<br><br>
+	<?php endif; ?>
 </section>

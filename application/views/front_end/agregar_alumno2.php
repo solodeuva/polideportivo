c94d0aa21f84ae88890a71703edee8ba191e3f7d
keepercito <keepercito@gmail.com>
2016-02-25 16:46:04 -0600

    validacion de fecha al ingresar un alumno nuevo y reacomodo de unos botones

--- application/views/front_end/agregar_alumno2.php
+++ application/views/front_end/agregar_alumno2.php
@@ -1,14 +1,7 @@
-<?php function calcular_edad($fecha){
-		$dias = explode('/', $fecha, 3);
-		$dias = mktime(0,0,0,$dias[1],$dias[0],$dias[2]);
-		$edad = (int)((time()-$dias)/31556926 );
-		return $edad;						
-}
-?>
 <section class="contenido">
 <?php if(isset($_SESSION['logged_in'])): #si ha iniciado sesion se mostrara el contenido sino el mensaje de error?>
-	<h1>Editar Alumno</h1>
-	<?= form_open('Alumnos/actualizarAlumno')?>
+	<h1>Datos del alumno</h1>
+	<?= form_open('Alumnos/agregarAlumno')?>
 		<table class="tabla">
 			<tbody>
 				<tr>
@@ -16,16 +9,16 @@
 					<td>Apellidos del alumno:</td>
 				</tr>
 				<tr>
-					<td><input name="nombres" maxlength="30" value="<?php echo $alumno->getNombres();?>" required/></td>
-					<td><input name="apellidos" maxlength="30" value="<?php echo $alumno->getApellidos();?>" required/></td>
+					<td><input name="nombres" maxlength="30" value="<?= $nombres;?>" required/></td>
+					<td><input name="apellidos" maxlength="30" value="<?= $apellidos;?>" required/></td>
 				</tr>
 				<tr>
 					<td>Estatura:</td>
 					<td>Peso:</td>
 				</tr>
 				<tr>
-					<td><input name="estatura" maxlength="4" size="3" value="<?php echo $alumno->getEstatura();?>"/></td>
-					<td><input name="peso" maxlength="3" size="2" value="<?php echo $alumno->getPeso();?>"/></td>
+					<td><input name="estatura" maxlength="4" size="3" value="<?= $estatura;?>"/></td>
+					<td><input name="peso" maxlength="3" size="2" value="<?= $peso;?>"/></td>
 				</tr>
 				<tr>
 					<td style="vertical-align:top">Ingrese estatura en metros (solo números).</td>
@@ -36,36 +29,14 @@
 					<td></td>
 				</tr>
 				<tr>
-					<td>Estado:</td>
-					<td>Edad:</td>
-				</tr>
-				<tr>
-					<td>
-						<select name="estado">
-						<?php if($alumno->getEstado() == 'A'): ?>
-							<option value="A" selected="Activo">Activo</option>
-							<option value="B">Inactivo</option>
-						<?php else: ?>
-							<option value="A">Activo</option>
-							<option value="B" selected="Inactivo">Inactivo</option>
-						<?php endif;?>
-						</select>
-					</td>
-					<td><?php echo calcular_edad($alumno->getFechaNacimiento());?></td>
-				</tr>
-				<tr>
-					<td>&nbsp;</td><!-- Esto es para dejar un espacio entre filas-->
-					<td></td>
-				</tr>
-				<tr>
 					<td>Fecha de nacimiento:</td>
 					<td>Género:</td>
 				</tr>
 				<tr>
-					<td><input name="fnacimiento" maxlength="10" size="10" value="<?php echo $alumno->getFechaNacimiento();?>" required/></td>				
+					<td><input name="fnacimiento" maxlength="10" size="10" value="<?= $fnacimiento;?>" required/></td>				
 					<td>
 						<select name="genero">
-						<?php if($alumno->getGenero() == 'M'): ?>
+						<?php if($genero == 'M'): ?>
 							<option value="M" selected="Masculino">Masculino</option>
 							<option value="F">Femenino</option>
 						<?php else: ?>
@@ -74,6 +45,9 @@
 						<?php endif;?>
 						</select>
 					</td>
+				</tr>
+				<tr>
+					<td style="vertical-align:top">Fecha ingresada invalida</td>
 				</tr>
 				<tr>
 					<td style="vertical-align:top">Utilice el formato: 17/02/2016</td>
@@ -87,8 +61,8 @@
 					<td>Teléfono:</td>
 				</tr>
 				<tr>
-					<td><input name="dir" maxlength="100" value="<?php echo $alumno->getDireccion();?>" /></td>
-					<td><input name="tel" maxlength="9" value="<?php echo $alumno->getTelefono();?>" size="7"/></td>
+					<td><input name="dir" maxlength="100" value="<?= $dir;?>" /></td>
+					<td><input name="tel" maxlength="9" value="<?= $tel;?>" size="7"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -103,11 +77,11 @@
 					<td>Nivel:</td>
 				</tr>
 				<tr>
-					<td><textarea name="exp" cols="40" rows="10" maxlength="250"><?php echo $alumno->getExpPrevia();?></textarea></td>
+					<td><textarea name="exp" cols="40" rows="10" maxlength="250"><?= $exp;?></textarea></td>
 					<td style="vertical-align:top">
 						<select name="nivel">
-						<?php foreach ($nivel as $n) { ?>
-							<?php if($alumno->getIdNivel()->getIdNivel() == $n->getIdNivel()): ?>
+						<?php foreach ($niveles as $n) { ?>
+							<?php if($nivel == $n->getIdNivel()): ?>
 								<option value="<?= $n->getIdNivel();?>" selected="<?php echo $n->getNombre();?>"><?php echo $n->getNombre();?></option>
 							<?php else: ?>
 								<option value="<?= $n->getIdNivel();?>"><?php echo $n->getNombre();?></option>
@@ -125,8 +99,8 @@
 					<td>DUI de la madre:</td>
 				</tr>
 				<tr>
-					<td><input name="madre" maxlength="60" value="<?php echo $alumno->getNombreMadre();?>" /></td>
-					<td><input name="duim" maxlength="10" size="7" value="<?php echo $alumno->getDuiMadre();?>"/></td>
+					<td><input name="madre" maxlength="60" value="<?= $madre;?>" /></td>
+					<td><input name="duim" maxlength="10" size="7" value="<?= $duim;?>"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -141,8 +115,8 @@
 					<td>Teléfono de la madre:</td>
 				</tr>
 				<tr>
-					<td><input name="tbjm" maxlength="100" value="<?php echo $alumno->getTrabajoMadre();?>" /></td>
-					<td><input name="telm" maxlength="9" value="<?php echo $alumno->getTelMadre();?>" size="7"/></td>
+					<td><input name="tbjm" maxlength="100" value="<?= $tbjm;?>" /></td>
+					<td><input name="telm" maxlength="9" value="<?= $telm;?>" size="7"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -157,8 +131,8 @@
 					<td>DUI del padre:</td>
 				</tr>
 				<tr>
-					<td><input name="padre" maxlength="60" value="<?php echo $alumno->getNombrePadre();?>" /></td>
-					<td><input name="duip" maxlength="10" size="7" value="<?php echo $alumno->getDuiPadre();?>"/></td>
+					<td><input name="padre" maxlength="60" value="<?= $padre;?>" /></td>
+					<td><input name="duip" maxlength="10" size="7" value="<?= $duip;?>"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -173,8 +147,8 @@
 					<td>Teléfono del padre:</td>
 				</tr>
 				<tr>
-					<td><input name="tbjp" maxlength="100" value="<?php echo $alumno->getTrabajoPadre();?>" /></td>
-					<td><input name="telp" maxlength="9" value="<?php echo $alumno->getTelPadre();?>" size="7"/></td>
+					<td><input name="tbjp" maxlength="100" value="<?= $tbjp;?>" /></td>
+					<td><input name="telp" maxlength="9" value="<?= $telp;?>" size="7"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -196,8 +170,8 @@
 					<td>DUI del responsable:</td>
 				</tr>
 				<tr>
-					<td><input name="resp" maxlength="60" value="<?php echo $alumno->getNombreResp();?>" /></td>
-					<td><input name="duir" maxlength="10" value="<?php echo $alumno->getDuiResp();?>" size="7"/></td>
+					<td><input name="resp" maxlength="60" value="<?= $resp;?>" /></td>
+					<td><input name="duir" maxlength="10" value="<?= $duir;?>" size="7"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -212,8 +186,8 @@
 					<td>Teléfono del Responsable:</td>
 				</tr>
 				<tr>
-					<td><input name="tbjr" maxlength="100" value="<?php echo $alumno->getTrabajoResp();?>" /></td>
-					<td><input name="telr" maxlength="9" value="<?php echo $alumno->getTelResp();?>" size="7"/></td>
+					<td><input name="tbjr" maxlength="100" value="<?= $tbjr;?>" /></td>
+					<td><input name="telr" maxlength="9" value="<?= $telr;?>" size="7"/></td>
 				</tr>
 				<tr>
 					<td></td>
@@ -228,32 +202,22 @@
 					<td>Medicinas:</td>
 				</tr>
 				<tr>
-					<td><textarea name="padecimiento" cols="40" rows="10" maxlength="200"><?php echo $alumno->getPadecimientos();?></textarea></td>
-					<td><textarea name="medic" cols="40" rows="10" maxlength="200"><?php echo $alumno->getMedicamentos();?></textarea></td>
+					<td><textarea name="padecimiento" cols="40" rows="10" maxlength="200"><?= $padecimiento;?></textarea></td>
+					<td><textarea name="medic" cols="40" rows="10" maxlength="200"><?= $medic;?></textarea></td>
 				</tr>
 				<tr>
-					<td><input type="hidden" value="<?php echo $alumno->getIdAlumno();?>" name="id"/></td>
+					<td>&nbsp;</td>
 				</tr>
 			</tbody>
 		</table>
+	<br>	
+	<?= form_submit('','Ingresar Alumno')?>
+	<?= form_close()?>
 	<br>
+		<form action="<?php echo base_url('alumnos/gestionarAlumnos');?>">
+			<input type="submit" value="Regresar"/>
+		</form>
 	<br>
-	<table>
-		<tbody>
-			<tr><?= form_submit('','Actualizar Alumno')?>
-				<?= form_close()?>
-				<form action="<?php echo base_url('alumnos/buscarAlumnos');?>">
-				<input type="submit" value="Regresar a Buscar"/>
-				</form>
-			</tr>
-			<tr>
-				<td>&nbsp;</td>
-			</tr>
-			<tr>
-				<center><input type="button" value="Eliminar Alumno" onclick="eliminarAlumno(<?php echo $alumno->getIdAlumno();?>)"></center>
-			</tr>
-		</tbody>
-	</table>
 <?php else: ?>
 	<br><br><br><br><br><br><br>
 	<h2>Tu sesión expiró o no has iniciado sesión, por favor inicia sesión para ver este contenido</h2>
